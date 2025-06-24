d91f093d6d5ebdcdcb564b1360885ab0055adb37
Ali Talib <101087513+alit644@users.noreply.github.com>
2025-06-24 20:27:24 +0300

    Update useAuthStore.ts6

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -89,7 +89,7 @@ export const useAuthStore = create<IAuthStore>((set) => ({
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: "products-manger-supabse.vercel.app",
+          redirectTo: "https://products-manger-supabse.vercel.app/",
         },
       });
       if (error) {
@@ -110,7 +110,7 @@ export const useAuthStore = create<IAuthStore>((set) => ({
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "github",
         options: {
-          redirectTo: "products-manger-supabse.vercel.app",
+          redirectTo: "https://products-manger-supabse.vercel.app/",
         },
       });
       if (error) {
