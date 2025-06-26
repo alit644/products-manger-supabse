e1aaa4764bb53e0232051587fc3cb7a9fb94388e
Ali Talib <101087513+alit644@users.noreply.github.com>
2025-06-26 17:39:18 +0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,6 +15,7 @@ const Sidebar = () => {
   const renderLink = SIDEBAR_DATA.map((link) => (
     <li key={link.title}>
       <NavLink
+        onClick={() => setIsOpen(false)}
         to={link.path}
         className={
           "flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
